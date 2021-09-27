10eecab4b2c22411eead33571075ab7ba6987a0a
yihong li <Helenalyh>
2021-09-26 23:44:48 -0400

    Created an dashboard to create/edit/delete reviewers

--- mayan/apps/reviewers/models.py
+++ mayan/apps/reviewers/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from mayan.apps.databases.model_mixins import ExtraDataModelMixin
+from mptt.models import MPTTModel
 
-# Create your models here.
+class Dashboard(ExtraDataModelMixin, MPTTModel):
+    pass
+
+class Candidate(ExtraDataModelMixin, MPTTModel):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    DOB = models.DateField()
+    dashboard = models.ForeignKey(Dashboard,on_delete=models.CASCADE)
+
+class Reviewers(ExtraDataModelMixin, MPTTModel):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    DOB = models.DateField()
+    dashboard = models.ForeignKey(Dashboard,on_delete=models.CASCADE)
