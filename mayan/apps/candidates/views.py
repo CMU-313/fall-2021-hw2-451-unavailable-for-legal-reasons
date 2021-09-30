5c79cb9a089340c1825fb385fa43c9414fca9336
Dsm0 <willrinkoff@gmail.com>
2021-09-30 18:55:46 -0400

    implemented a sql error

--- mayan/apps/candidates/views.py
+++ mayan/apps/candidates/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from mayan.apps.views.generics import SingleObjectListView
 from django.http import HttpResponse
 
-from .models import Candidate
+from .models import ModelCandidate_django, ModelCandidateManager_django
 
 from django.http import HttpResponse
 
@@ -15,4 +15,20 @@ class CandidatesView(SingleObjectListView):
     pass
 
 def table(request):
+    basic_info = {
+        "name":"foof",
+        "email":"foof@troof.oof",
+        "phone_number":"6786786789",
+        "DOB":"none",
+        "undergrad_experience":"none"
+    }
+    heh = ModelCandidate_django.objects.create_candidate(basic_information=basic_info)
+    # heh.save()
+
+    # Getting all the stuff from database
+    # query_results = ModelCandidate_django.objects.all()
+
+    # Creating a dictionary to pass as an argument
+    # context = { 'query_results' : query_results }
+
     return render(request, "candidates/candidate_table.html")
