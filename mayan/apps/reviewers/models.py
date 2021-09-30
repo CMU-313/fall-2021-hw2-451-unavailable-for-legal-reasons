e5fd678cc35f68ac4304a26fa655545c1626112a
prithviokade <44682787+prithviokade@users.noreply.github.com>
2021-09-30 17:42:57 -0400

    Custom manager for creating reviewer objects

--- mayan/apps/reviewers/models.py
+++ mayan/apps/reviewers/models.py
@@ -16,10 +16,32 @@ class Reviewers(ExtraDataModelMixin, MPTTModel):
     assigned_candidates = models.CharField(max_length=50)
     dashboard = models.ForeignKey(Dashboard,on_delete=models.CASCADE)
 
+# previous python class for reviewer for reference
+class Reviewer(): 
+    def __init__(self, **reviewer_input): 
+        try:
+            self.name = reviewer_input["name"]
+        except:
+            raise Exception("Reviewer name is required.")
+        self.position = reviewer_input.get("position", "graduate_student")
+        self.employment_time = reviewer_input.get("employment_time", "undecided")
+        self.assigned_candidates = reviewer_input.get("assigned_candidates", [])
 
+# reference doc: https://docs.djangoproject.com/en/3.2/ref/models/instances/ 
+class ModelReviewerManager_django(models.Manager): 
+    def create_reviewer(self, **reviewer_input): 
+        try: 
+            name = reviewer_imput["name"]
+        except:
+            raise Exception("Reviewer name is required.")
+        position = reviewer_input.get("position", "graduate_student")
+        employment_time = reviewer_input.get("employment_time", "undecided")
+        assigned_candidates = reviewer_input.get("assigned_candidates", [])    
 
-# Create your models here.
-
+        reviewer = self.create(name=name, position=position, 
+                               employment_time=employment_time,
+                               assigned_candidates=assigned_candidates) 
+        return reviewer
 
 class ModelReviewer_django(models.Model): 
         name = models.CharField(max_length = 50)
@@ -34,3 +56,8 @@ class ModelReviewer_django(models.Model):
         )
         employment_time = models.CharField(max_length=2, choices=EMPLOYMENT_TIME)
         assigned_candidates = models.ManyToManyField('candidates.ModelCandidate_django')
+        
+        objects = ModelReviewerManager_django()
+        
+# create new reviewer object as
+# reviewer = ModelReviewer_django.objects.create_reviewer(< pass in inputs here>) 
